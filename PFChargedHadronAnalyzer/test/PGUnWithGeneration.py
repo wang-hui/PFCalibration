be22327b2c6cfda9e65f51547f8524bcdc4d3b91
schoefbeck <robert.schoefbeck@cern.ch>
2016-11-28 18:45:03 +0100

    fixing GT for 810pre16

--- PFChargedHadronAnalyzer/test/PGUnWithGeneration.py
+++ PFChargedHadronAnalyzer/test/PGUnWithGeneration.py
@@ -71,7 +71,8 @@ process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
 process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
 #process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')
-process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_TrancheIV_v6', '')
+#process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_TrancheIV_v6', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '81X_upgrade2017_realistic_v22', '')
 
 
 process.generator = cms.EDProducer("FlatRandomEGunProducer",
